dd939b60cb25db2145e357cdcf0a9f03506d8690
Allan Larangeiras <alarangeiras@gmail.com>
2024-07-30 22:34:36 -0300

    feat: add table

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { exec } from "child_process";
 import { promisify } from "util";
+import Table from 'cli-table';
 
 async function main() {
     const result = await promisify(exec)("sudo lsof -iTCP -sTCP:LISTEN -n -P");
-    console.log(result.stdout);
+    const rawResult = result.stdout;
+    const lines = rawResult.split("\n");
+    const table = new Table();
+    for (const line of lines) {
+        const parts = line.split(/\s+/);
+        if (parts[0] === '') {
+            continue;
+        }
+        table.push(parts);
+    }
+    console.log(table.toString());
 }
 
 main();
